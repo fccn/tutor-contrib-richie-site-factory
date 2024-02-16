88efb566a349992f160547fadae27b1ca6634eff
Henrry Pulgarin <39854568+Henrrypg@users.noreply.github.com>
2024-02-16 11:19:54 -0500

    fix: setup hooks to richie api (#4)

--- contrib/edx-platform/richie/settings/cms.py
+++ contrib/edx-platform/richie/settings/cms.py
@@ -2,6 +2,7 @@ def plugin_settings(settings):
     """Common settings for Richie catalog connector"""
     settings.RICHIE_COURSE_HOOK = {
         "secret": "richiesecret",
-        "url": "http://richie:8000/api/v1.0/course-runs-sync/",
+        "sync_endpoint": "http://richie:8000/api/v1.0/course-runs-sync/",
+        "create_endpoint": "http://richie:8000/api/v1.0/unesco-course-sync/",
         "timeout": 3,
     }
