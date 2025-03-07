71e0c71e0faf5018b2e6c977713693bf3262631a
Ivo Branco <ivo.branco@fccn.pt>
2025-03-07 09:48:40 +0000

    docs: review python version

--- setup.py
+++ setup.py
@@ -51,9 +51,7 @@ setup(
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
 )
